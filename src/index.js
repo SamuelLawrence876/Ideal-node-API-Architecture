644d055b7a0d55ac6529a91055467c99df62cf83
Samuel Lawrence <55727214+SamuelLawrence876@users.noreply.github.com>
2022-05-13 01:23:13 +0000

    docs

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 const express = require("express");
-// *** ADD ***
 const bodyParser = require("body-parser");
 const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
-
+const { swaggerDocs: V1SwaggerDocs } = require("./v1/swagger");
+const apicache = require("apicache");
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-// *** ADD ***
+const cache = apicache.middleware;
 app.use(bodyParser.json());
 app.use("/api/v1/workouts", v1WorkoutRouter);
-
+app.use(cache("2 minutes"));
 app.listen(PORT, () => {
   console.log(`API is listening on port ${PORT}`);
+  V1SwaggerDocs(app, PORT);
 });
--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,8 +1,29 @@
 const express = require("express");
 const workoutController = require("../../controllers/workoutController");
-const recordController = require("../../controllers/recordController");
 const router = express.Router();
 
+/**
+ * @openapi
+ * /api/v1/workouts:
+ *   get:
+ *     tags:
+ *       - Workouts
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ */
 router.get("/", workoutController.getAllWorkouts);
 
 router.get("/:workoutId", workoutController.getOneWorkout);
@@ -14,3 +35,9 @@ router.patch("/:workoutId", workoutController.updateOneWorkout);
 router.delete("/:workoutId", workoutController.deleteOneWorkout);
 
 module.exports = router;
+
+// todo authentication
+// const authenticate = require("../../middlewares/authenticate");
+// const authorize = require("../../middlewares/authorize");
+
+// router.post("/", authenticate, authorize, workoutController.createNewWorkout);
